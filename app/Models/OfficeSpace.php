9c7e6e211d6110377b6657e7aa4f7423eb0cbaf1
Tegar Saputra <tegar0651@gmail.com>
2025-03-03 08:23:08 +0700

    penambahan database dan filament

---
+++ app/Models/OfficeSpace.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\OfficeSpacePhoto;
+use App\Models\City;
+use Illuminate\Support\Str;
+
+class OfficeSpace extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'thumbnail',
+        'is_open',
+        'is_full_booked',
+        'price',
+        'duration',
+        'address',
+        'about',
+        'slug',
+        'city_id',
+    ];
+
+    public function setNameAttribure($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+    public function photos(): HasMany
+    {
+        return $this->hasMany(OfficeSpacePhoto::class);
+    }
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(OfficeBenefit::class);
+    }
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+}
