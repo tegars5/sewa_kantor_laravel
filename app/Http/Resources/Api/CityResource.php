6ae4467d06ce85c7955722de67c42f01f425c3c6
Tegar Saputra <tegar0651@gmail.com>
2025-03-08 00:27:53 +0700

    menambahkan Api Json Resource

---
+++ app/Http/Resources/Api/CityResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use App\Filament\Resources\OfficeSpaceResource;
+use App\Models\OfficeSpace;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CityResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'photo' => $this->photo,
+            'officeSpace_count' => $this->office_space_count,
+            'officeSpace' => OfficeSpaceResource::collection($this->whenLoaded('officeSpace')),
+        ];
+    }
+}
